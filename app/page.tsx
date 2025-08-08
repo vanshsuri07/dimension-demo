5859e3bc0dad09874c940843b2a00af4a0b9d23c
Vansh Suri <vanshsuri07@gmail.com>
2025-08-08 16:50:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
    <div className="relative overflow-hidden">
-      <StarCanvas /> {/* Renders in background */}
+      <StarCanvas />
       <Header />
       <Hero />
       <AnimatedPage2 />
